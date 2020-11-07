ced9ef44eaf56a4dd7abd3f00b8adbca9257c422
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2020-11-07 13:09:39 -0500

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ybenitezf/flask_cognitologin',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
